193c192ea7a535e64416f2da26faa7782e98d7f0
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2025-01-05 16:32:53 +0900

    😏 라우팅 및 컴포넌트 구조

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
+<!-- script setup : Vue 3에서 도입된 기능, setup() 함수를 사용해 컴포넌트 옵션 정의, 이를 통해 템플릿에서 자동으로 해결되고 제공되는 종속성 가져올 수 있음 -->
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// @ 기호는 주로 모듈 번들러에서 사용되는 특별한 경로 별칭(alias)
+// 웹 애플리케이션을 번들링하기 위해 사용되는 도구, 여러 개의 JS 파일 및 리소스를 하나의 번들 파일로 결합하여 페이지 로딩 속도를 최적화하는 데 도움을 줌
+import HeaderSection from "@/components/HeaderSection.vue";
+import FooterSection from "@/components/FooterSection.vue";
+import IntroSection from "@/components/IntroSection.vue";
+import SkillSection from "@/components/SkillSection.vue";
+import SiteSection from "@/components/SiteSection.vue";
+import PortSection from "@/components/PortSection.vue";
+import ContactSection from "@/components/ContactSection.vue";
+import SkipSection from "@/components/SkipSection.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <SkipSection />
+    <HeaderSection />
+    <main id="main" role="main">
+        <IntroSection />
+        <SkillSection />
+        <SiteSection />
+        <PortSection />
+        <ContactSection />
+    </main>
+    <FooterSection />
 </template>
